a3e2699d9542ce800250e8c88b8d2cff6b533d8b
oyekamal <autocratickamal1@gmail.com>
2022-11-21 03:47:31 -0800

    CRUD is done

--- app.py
+++ app.py
@@ -12,17 +12,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(basedir, 'db.sqlite3')
 
 
-
 # configration attachment
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-
 # Creation of the database tables within the application context.
 with app.app_context():
     db.create_all()
-    # Creating database use 
+    # Creating database use
     # 1 flask --app app shell
     # 2 from app import db
     # 3 db.create_all()
@@ -39,6 +37,8 @@ class NoteModel(db.Model):
         self.content = content
 
 # same as serializer in django
+
+
 class NoteSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = NoteModel
@@ -47,10 +47,12 @@ class NoteSchema(ma.SQLAlchemyAutoSchema):
 note_schema = NoteSchema()
 notes_schema = NoteSchema(many=True)
 
+
 @app.route('/note/', methods=['GET'])
 def get_note():
     all_notes = NoteModel.query.all()
     return jsonify(notes_schema.dump(all_notes))
+
 
 @app.route('/note/', methods=['POST'])
 def create_note():
@@ -62,20 +64,30 @@ def create_note():
     return note_schema.jsonify(note)
 
 
-# @app.route('/note/<int:note_id>/', methods=['GET'])
-# def note_detail(note_id):
-#     note = NoteModel.query.get(note_id)
-#     return jsonify(note)
+@app.route('/note/<int:note_id>/', methods=['GET'])
+def note_detail(note_id):
+    note = NoteModel.query.get(note_id)
+    return note_schema.jsonify(note)
 
-# @app.route('/note/<int:note_id>/', methods=['GET'])
-# def update_note(note_id):
-#     note = NoteModel.query.get(note_id)
-#     title = request.json.get("title")
-#     content = request.json.get("content")
-#     note.title = title
-#     note.content = content
-#     db.session.add(note)
-#     db.session.commit()
+
+@app.route('/note/<int:note_id>/', methods=['PATCH'])
+def update_note(note_id):
+    note = NoteModel.query.get(note_id)
+    title = request.json.get("title")
+    content = request.json.get("content")
+    note.title = title
+    note.content = content
+    db.session.add(note)
+    db.session.commit()
+    return note_schema.jsonify(note)
+
+
+@app.route('/note/<int:note_id>/', methods=['DELETE'])
+def delete_note(note_id):
+    note = NoteModel.query.get(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return note_schema.jsonify(note)
 
 
 @app.route('/')
